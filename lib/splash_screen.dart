5bbc345ff5715c24b6f8ae2ccea5ff3c74251442
Carl Emad <carlemad293@gmail.com>
2024-08-19 07:32:03 +0300

    Merge remote-tracking branch 'origin/master'

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -11,6 +11,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   late AnimationController _controller;
   late Animation<double> _animation;
   late String randomVerse;
+  late LinearGradient gradient;
 
   final List<String> englishVerses = [
     "Pray without ceasing. (1 Thessalonians 5:17)",
@@ -48,13 +49,17 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     );
     _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
 
-    _controller.forward(); // Start fade-in animation
-    Future.delayed(const Duration(seconds: 5), () { // Increase duration to 5 seconds
+    _controller.forward();
+    Future.delayed(const Duration(seconds: 5), () {
       _controller.reverse().then((_) {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => HomeScreen()),
         );
       });
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {});
     });
   }
 
@@ -62,12 +67,28 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   void didChangeDependencies() {
     super.didChangeDependencies();
 
-    // Determine app language
-    bool isArabic = Localizations.localeOf(context).languageCode == 'ar';
+    final bool isDarkTheme = Theme.of(context).brightness == Brightness.dark;
+    gradient = _getGradient(isDarkTheme);
 
-    // Select a random verse based on the language
+    bool isArabic = Localizations.localeOf(context).languageCode == 'ar';
     final List<String> verses = isArabic ? arabicVerses : englishVerses;
     randomVerse = verses[Random().nextInt(verses.length)];
+  }
+
+  LinearGradient _getGradient(bool isDarkTheme) {
+    if (isDarkTheme) {
+      return LinearGradient(
+        colors: [Colors.blueGrey.shade900, Colors.black],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      );
+    } else {
+      return LinearGradient(
+        colors: [Colors.lightBlue.shade300, Colors.white],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      );
+    }
   }
 
   @override
@@ -78,44 +99,92 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
 
   @override
   Widget build(BuildContext context) {
+    final bool isDarkTheme = Theme.of(context).brightness == Brightness.dark;
+    final Color borderColor = isDarkTheme ? Colors.white.withOpacity(0.6) : Colors.white;
+    final Color textColor = isDarkTheme ? Colors.white : Colors.black;
+    final List<BoxShadow> shadows = isDarkTheme
+        ? [
+      BoxShadow(
+        color: Colors.blue.shade900.withOpacity(0.5),
+        blurRadius: 20,
+        spreadRadius: 10,
+      ),
+      BoxShadow(
+        color: Colors.blue.shade700.withOpacity(0.5),
+        blurRadius: 10,
+        spreadRadius: 5,
+      ),
+    ]
+        : [
+      BoxShadow(
+        color: Colors.white.withOpacity(0.7),
+        blurRadius: 30,
+        spreadRadius: 15,
+      ),
+      BoxShadow(
+        color: Colors.white.withOpacity(0.5),
+        blurRadius: 20,
+        spreadRadius: 10,
+      ),
+    ];
+
     return Scaffold(
-      body: Center(
-        child: FadeTransition(
-          opacity: _animation,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 300,
-                height: 400,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 4.0,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: gradient,
+        ),
+        child: Center(
+          child: FadeTransition(
+            opacity: _animation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: borderColor,
+                      width: 4.0,
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                    boxShadow: shadows,
                   ),
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(25.0),
-                  child: Image.asset(
-                    'assets/splash_image.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20), // Space between the image and verse
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  randomVerse,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Image.asset(
+                      'assets/splash_image.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    randomVerse,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: textColor,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: isDarkTheme ? Colors.blue.shade900 : Colors.white,
+                          offset: Offset(0, isDarkTheme ? 2 : -2),
+                        ),
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: isDarkTheme ? Colors.blue.shade700 : Colors.white,
+                          offset: Offset(0, isDarkTheme ? -2 : 2),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
